ee2fa7a863c565ab59f20c53e5180946f422f015
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-13 13:39:14 +0300

    test hw-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+const day = 3;
+
+switch (day) {
+  case 'red':
+    console.log('You color red');
+    break;
+  case 'blue':
+    console.log('You color blue');
+    break;
+  case 'grin':
+    console.log('You color grin');
+    break;
+
+  default:
+    console.log('Not error');
+
+    break;
+}
