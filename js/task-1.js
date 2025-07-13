f4a0821011e29e1796a7be1b43ddb2f54fe4755d
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-13 13:43:16 +0300

    test hw-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,18 @@
-const day = 3;
+const day = 6;
 
 switch (day) {
-  case 'red':
-    console.log('You color red');
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log('This is a working day');
     break;
-  case 'blue':
-    console.log('You color blue');
-    break;
-  case 'grin':
-    console.log('You color grin');
+  case 6:
+  case 7:
+    console.log('It is a day off');
     break;
 
   default:
-    console.log('Not error');
-
-    break;
+    console.log('Invalid');
 }
