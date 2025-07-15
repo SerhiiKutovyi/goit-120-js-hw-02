dfc8d008a73bb723b95f1c7cd5700837d112d77b
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-15 16:52:38 +0300

    test hw-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,44 @@
-const day = 6;
+// const day = 6;
 
-switch (day) {
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-    console.log('This is a working day');
-    break;
-  case 6:
-  case 7:
-    console.log('It is a day off');
-    break;
+// switch (day) {
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log('This is a working day');
+//     break;
+//   case 6:
+//   case 7:
+//     console.log('It is a day off');
+//     break;
 
-  default:
-    console.log('Invalid');
+//   default:
+//     console.log('Invalid');
+// }
+
+function getSubscriptionPrice(type) {
+  switch (type) {
+    case 'starter':
+      console.log('0');
+
+      break;
+
+    case 'professional':
+      console.log('20');
+
+      break;
+
+    case 'organization':
+      console.log('50');
+
+      break;
+
+    default:
+      console.log('Invalid subscription type!');
+  }
+  return type;
 }
+
+getSubscriptionPrice('starter');
+getSubscriptionPrice('random');
